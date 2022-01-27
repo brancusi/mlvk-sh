c3413ce024f123bfd66ed4ec9d0527034db54b28
Aram Zadikian <brancusi@gmail.com>
2022-01-27 12:48:04 -0800

    adding toml

--- app/root.jsx
+++ app/root.jsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "remix";
 
-import styles from "./styles/tailwind.css";
+import styles from "../styles/tailwind.css";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
